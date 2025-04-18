087cda7b500250a1b2461ebcfcf81dd2f6a9c598
Phuongpham2406 <phamtphuong2406@gmail.com>
2025-04-18 15:34:24 +0700

    delete SendEmailcontroller

--- Transportation/obj/Debug/net8.0/Transportation.AssemblyInfo.cs
+++ Transportation/obj/Debug/net8.0/Transportation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transportation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11b2a8ecf448dbb97294725e889b79ed3153a46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46fe1fd2de37597272ca00ab4698eb937a385a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transportation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transportation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
